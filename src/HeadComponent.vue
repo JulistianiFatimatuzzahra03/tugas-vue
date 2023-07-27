4341dee066a745093969853f083d2a157982c4b4
JulistianiFatimatuzzahra03 <21221753120@smkassalaambandung.sch.id>
2023-07-27 14:31:48 +0700

    latvue

--- src/HeadComponent.vue
+++ src/HeadComponent.vue
@@ -1,6 +1,6 @@
 <template>
   
-<section class="bg-center bg-no-repeat bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Realme-realme-_logo_box-RGB-01.svg/800px-Realme-realme-_logo_box-RGB-01.svg.png')] bg-gray-700 bg-blend-multiply">
+<section class="bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Realme-realme-_logo_box-RGB-01.svg/1250px-Realme-realme-_logo_box-RGB-01.svg.png')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Realme</h1>
         <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Kekurangan dan kelebihan dari HP Realme</p>
--- src/Realme1Component.vue
+++ src/Realme1Component.vue
@@ -2,16 +2,16 @@
     
 <div class="grid grid-cols-2 gap-2">
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1689824668.webp" alt="150">
+           <center> <p class="max-w-lg text-3xl font-semibold leading-relaxed text-gray-900 dark:text-white">Realme 11 Pro 5G</p><img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1689824668.webp" alt="150"></center>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1648806764.webp" alt="150">
+        <center>    <p class="max-w-lg text-3xl font-semibold leading-relaxed text-gray-900 dark:text-white">Realme GT 2 Pro</p><img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1648806764.webp" alt="150"></center>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1688467070.webp" alt="150">
+            <center><p class="max-w-lg text-3xl font-semibold leading-relaxed text-gray-900 dark:text-white">Realme C53 NFC</p> <img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1688467070.webp" alt="150"></center>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1646378813.webp" alt="">
+            <center><p class="max-w-lg text-3xl font-semibold leading-relaxed text-gray-900 dark:text-white">Realme 9 Pro +</p><img class="h-auto max-w-full rounded-lg" src="https://media.pricebook.co.id/article/61653d84b42c2ed78516449d/61653d84b42c2ed78516449d_1646378813.webp" alt=""></center>
         </div>
     </div>
 
